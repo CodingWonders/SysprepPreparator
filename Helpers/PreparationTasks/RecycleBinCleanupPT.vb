271d5835cddc89c0809bb97275c008933b726b66
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-08-30 21:53:49 +0200

    [PT/RecBinPT] Use LINQ query, fill in some info, fix some stuff

--- Helpers/PreparationTasks/RecycleBinCleanupPT.vb
+++ Helpers/PreparationTasks/RecycleBinCleanupPT.vb
@@ -19,19 +19,17 @@ Namespace Helpers.PreparationTasks
             DynaLog.LogMessage("Clearing the Recycle Bin of the current user...")
             If IsInTestMode Then Return True
             Try
-                Dim drives() As DriveInfo = DriveInfo.GetDrives()
-                For Each drive As DriveInfo In drives
-                    If drive.IsReady Then
-                        Dim recycleBinPath As String = Path.Combine(drive.RootDirectory.FullName, GetUserSid(Environment.GetEnvironmentVariable("USERNAME")))
-                        Try
-                            RemoveRecursive(recycleBinPath)
-                        Catch ex As Exception
-                        End Try
-                    End If
+                For Each drive As DriveInfo In DriveInfo.GetDrives().Where(Function(driveInList) driveInList.IsReady).ToList()
+                    Dim recycleBinPath As String = Path.Combine(drive.RootDirectory.FullName, GetUserSid(Environment.GetEnvironmentVariable("USERNAME")))
+                    Try
+                        RemoveRecursive(recycleBinPath)
+                    Catch ex As Exception
+                        Continue For
+                    End Try
                 Next
-                Return 1
+                Return True
             Catch ex As Exception
-                Return 0
+                Return False
             End Try
         End Function
     End Class
