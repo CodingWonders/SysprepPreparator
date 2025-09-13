90ba4b833be9023df47369d918e5b02d80ef6c18
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-09-13 17:09:48 +0200

    [Project] Automate release packing during build

--- PackRelease.ps1
+++ PackRelease.ps1
@@ -1,9 +1,12 @@
 if (Test-Path -Path "$((Get-Location).Path)\bin\Debug\SysprepPreparator.exe" -PathType Leaf) {
-	New-Item -Path ".\out" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item -Path ".\bin\Debug\Microsoft.Dism.dll" -Destination ".\out\Microsoft.Dism.dll" -Verbose -Force
-	Copy-Item -Path ".\bin\Debug\IniFileParser.dll" -Destination ".\out\IniFileParser.dll" -Verbose -Force
-	New-Item -Path ".\out\Languages" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item -Path ".\bin\Debug\Languages\*.ini" -Destination ".\out\Languages" -Verbose -Force
-	Copy-Item -Path ".\bin\Debug\SysprepPreparator.exe*" -Destination ".\out" -Verbose -Force
-	Compress-Archive -Path ".\out\*" -DestinationPath ".\SysprepPreparator.zip" -Force
+	Write-Host "Packing release..."
+	New-Item -Path "$((Get-Location).Path)\out" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+	Copy-Item -Path "$((Get-Location).Path)\bin\Debug\Microsoft.Dism.dll" -Destination "$((Get-Location).Path)\out\Microsoft.Dism.dll" -Verbose -Force
+	Copy-Item -Path "$((Get-Location).Path)\bin\Debug\IniFileParser.dll" -Destination "$((Get-Location).Path)\out\IniFileParser.dll" -Verbose -Force
+	New-Item -Path "$((Get-Location).Path)\out\Languages" -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+	Copy-Item -Path "$((Get-Location).Path)\bin\Debug\Languages\*.ini" -Destination "$((Get-Location).Path)\out\Languages" -Verbose -Force
+	Copy-Item -Path "$((Get-Location).Path)\bin\Debug\SysprepPreparator.exe*" -Destination "$((Get-Location).Path)\out" -Verbose -Force
+	Compress-Archive -Path "$((Get-Location).Path)\out\*" -DestinationPath "$((Get-Location).Path)\SysprepPreparator.zip" -Force
+} else {
+	Write-Host "Output directory not found."
 }
