058092044ff16353d741fe594846d1a2b859de45
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2026-07-25 18:13:39 +0200

    [CCP] Add check for BitLocker volumes

---
+++ Helpers/CompatChecks/BitLockerCCP.vb
@@ -0,0 +1,65 @@
+﻿Imports BDELib.BDELib
+Imports BDELib.Classes
+
+Namespace Helpers.CompatChecks
+
+    Public Class BitLockerCCP
+        Inherits CompatibilityCheckerProvider
+
+        Public Overrides Function PerformCompatibilityCheck() As Classes.CompatibilityCheckerProviderStatus
+            Dim SystemPersistentVolumeId As String = GetPersistentVolumeIdFromSystemVolume()
+            ' If we have no persistent volume ID, then we know that the volume is not encrypted
+            If SystemPersistentVolumeId = "" Then
+                Status.Compatible = True
+                Status.StatusMessage = New Classes.StatusMessage("BitLocker Drive Encryption Status on System Volume",
+                                                                 "The system volume is not encrypted with BitLocker.",
+                                                                 Classes.StatusMessage.StatusMessageSeverity.Info)
+                Return Status
+            End If
+
+            Dim systemVolumeConversionStatus As ConversionStatus = GetVolumeConversionStatus(SystemPersistentVolumeId)
+            If systemVolumeConversionStatus Is Nothing Then
+                Status.Compatible = False
+                Status.StatusMessage = New Classes.StatusMessage("BitLocker Drive Encryption Status on System Volume",
+                                                                 "The system volume is encrypted with BitLocker, but we could not get information about it. Sysprep will fail to validate your installation.",
+                                                                 Classes.StatusMessage.StatusMessageSeverity.Critical)
+                Return Status
+            End If
+
+            Status.Compatible = False
+
+            Dim blStatusStr As String = ""
+            Select Case systemVolumeConversionStatus.ConversionStatus
+                Case VolumeConversionStatus.FullyDecrypted : blStatusStr &= String.Format("- Volume Status: Fully Decrypted{0}", Environment.NewLine)       ' uhhh, what? this is not possible!
+                Case VolumeConversionStatus.FullyEncrypted : blStatusStr &= String.Format("- Volume Status: Fully Encrypted{0}", Environment.NewLine)
+                Case VolumeConversionStatus.DecryptionInProgress : blStatusStr &= String.Format("- Volume Status: Decryption in progress{0}", Environment.NewLine)
+                Case VolumeConversionStatus.EncryptionInProgress : blStatusStr &= String.Format("- Volume Status: Encryption in progress{0}", Environment.NewLine)
+                Case VolumeConversionStatus.DecryptionPaused : blStatusStr &= String.Format("- Volume Status: Decryption paused{0}", Environment.NewLine)
+                Case VolumeConversionStatus.EncryptionPaused : blStatusStr &= String.Format("- Volume Status: Encryption paused{0}", Environment.NewLine)
+            End Select
+
+            Dim divider As Integer = 10000
+
+            blStatusStr &= String.Format("- % Encrypted: {0}%", Math.Round(systemVolumeConversionStatus.EncryptionPercentage / divider, 2))
+
+            Status.StatusMessage = New Classes.StatusMessage("BitLocker Drive Encryption Status on System Volume",
+                                                             "The system volume is encrypted with BitLocker. Sysprep will fail to validate your installation.",
+                                                             String.Format("This is the current encryption state of the system volume:{0}{1}{0}You must decrypt this volume by using the Control Panel or manage-bde. You don't need to restart your computer. After it is fully done, refresh the checks.", Environment.NewLine, blStatusStr),
+                                                             Classes.StatusMessage.StatusMessageSeverity.Critical)
+            Return Status
+        End Function
+
+        Private Function GetPersistentVolumeIdFromSystemVolume() As String
+            Dim PersistentVolumeId As String = ""
+
+            Dim EncryptedVolumeMOC As ManagementObjectCollection = WMIHelper.GetResultsFromManagementQuery(String.Format("SELECT PersistentVolumeId FROM Win32_EncryptableVolume WHERE DriveLetter = {0}{1}{0}", Quote, WMIHelper.GetEscapedValue(Environment.GetEnvironmentVariable("SYSTEMDRIVE"))), "root\cimv2\Security\MicrosoftVolumeEncryption")
+            If EncryptedVolumeMOC Is Nothing Then Return PersistentVolumeId
+
+            PersistentVolumeId = WMIHelper.GetObjectValue(EncryptedVolumeMOC(0), "PersistentVolumeID")
+
+            Return PersistentVolumeId
+        End Function
+
+    End Class
+
+End Namespace
--- Tools/BDELib/My Project/AssemblyInfo.vb
+++ Tools/BDELib/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Revisar los valores de los atributos del ensamblado
 
-<Assembly: AssemblyTitle("Sysprep Preparation Tool")> 
-<Assembly: AssemblyDescription("The Sysprep Prep Tool prepares your computer for sysprep generalization and image capture")> 
+<Assembly: AssemblyTitle("BitLocker Drive Encryption Managed Library")> 
+<Assembly: AssemblyDescription("BitLocker Drive Encryption Managed Library")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("Sysprep Preparation Tool")>
-<Assembly: AssemblyCopyright("© 2025-2026 CodingWonders Software")>
+<Assembly: AssemblyProduct("BitLocker Drive Encryption Managed Library")> 
+<Assembly: AssemblyCopyright("© 2026 CodingWonders Software")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
-<Assembly: Guid("3a52d9f8-bd95-4680-bda9-929de2dd73df")>
+<Assembly: Guid("f835c126-7042-43e9-9a86-ac97d7fb70f4")> 
 
 ' La información de versión de un ensamblado consta de los cuatro valores siguientes:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el asterisco ('*'), como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.8.1.0")>
-<Assembly: AssemblyFileVersion("0.8.1.0")>
+<Assembly: AssemblyVersion("0.8.1.0")> 
+<Assembly: AssemblyFileVersion("0.8.1.0")> 
